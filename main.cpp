6ce0731a96893f9500acd3489cb4a64f97ac9741
mukeshbbx <74864792+mukeshbbx@users.noreply.github.com>
2020-11-23 21:57:30 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a,b;
+   cout<<" Enter the 1st value :";
+   cin>>a;
+   cout<<"Enter the 2nd Value :";
+   cin>>b;
+   cout<<"The Addition of the numbers is :"<<(a+b);
+    return 0;
+}
